799fc951169501570d09970076b8cfef8f55263f
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-10-13 19:47:56 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,9 @@
 def square_array(array)
-  numbers=[1,2,3]
-square_array=[]
-     counter = 0
-     while counter < numbers.length()
-     square_array.push(numbers[counter] * numbers[counter])
-     counter += 1
-     end
-     square_array
+new_array=[]
+counter=0
+while counter<array.length do
+  new_array<<array[counter]**2
+  counter+=1
+end
+return new_array
 end
